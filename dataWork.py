52546b4319fe1d20579d18ea061b918dab737333
Derg <94351943+Dergg@users.noreply.github.com>
2024-11-22 17:04:18 +0000

    Common verbs found
    Experimentation continues

--- dataWork.py
+++ dataWork.py
@@ -19,9 +19,9 @@ rdq = recordData.query('data_quality != "Needs Major Changes"')
 rdq = rdq[rdq['profile'].notnull()] # Filter down the data to stuff we can actually use
 rdq['profile'] = rdq['profile'].str.replace(r"\n", " ") # Remove the \n from the profiles so they don't cause any issues.
 rdq['all_hyperlinks'] = rdq['profile'].str.extract(r'\[(.*?)\]') # Regex to just get anything within square brackets.
-rdq[['hl_type', 'hyperlink_replace']] = rdq['all_hyperlinks'].str.extract(r'\[(\w+)(?:=([^\]]+))?\]') # Regex to get just names or numbers
-rdq['hl_rep'] = rdq.apply(lambda row: (row['hl_type'], row['all_hyperlinks']), axis=1) # Combine to get a tuple of (artist/label, name)
-print(rdq['hl_rep'].head(3))
+# rdq[['hl_type', 'hyperlink_replace']] = rdq['all_hyperlinks'].str.extract(r'\[(\w+)(?:=([^\]]+))?\]') # Regex to get just names or numbers
+# rdq['hl_rep'] = rdq.apply(lambda row: (row['hl_type'], row['all_hyperlinks']), axis=1) # Combine to get a tuple of (artist/label, name)
+# print(rdq['hl_rep'].head(3))
 
 tk = WhitespaceTokenizer()
 print("Tokenising profiles...")
@@ -34,7 +34,7 @@ rdq['profile_string'] = rdq['profile_tokenised'].apply(lambda x: ' '.join([item 
 # ^^ Above packages have already been downloaded.
 print("Tagging tokens...")
 if sys.argv[1] == "test":
-    rdq['tagged_tokens'] = rdq['profile_string'].head(10).progress_apply(lambda x: nltk.pos_tag(nltk.tokenize.word_tokenize(x)))
+    rdq['tagged_tokens'] = rdq['profile_string'].head(1000).progress_apply(lambda x: nltk.pos_tag(nltk.tokenize.word_tokenize(x)))
 elif sys.argv[1] == "full":
     rdq['tagged_tokens'] = rdq['profile_string'].progress_apply(lambda x: nltk.pos_tag(nltk.tokenize.word_tokenize(x)))
 print("Done.")
